208fef12de986e027f06ba235ea4ae52a222109b
Juan-Sergio Khoshdel <jsergiofm85@gmail.com>
2020-12-14 23:11:55 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -10,7 +10,7 @@ end
 
 def assign_rooms(speakers)
   speakers.map.with_index do
-    |name, room| "Hello, #{name}! You'll be assigned to room #{room+1}"
+    |name, room| "Hello, #{name}! You'll be assigned to room #{room+1}!"
   end
 end
 
