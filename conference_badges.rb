c5b4560e26459aca177c901e8f2c6787054472dd
Juan-Sergio Khoshdel <jsergiofm85@gmail.com>
2020-12-14 23:05:05 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -8,17 +8,13 @@ def batch_badge_creator(list)
   list.map {|name| "Hello, my name is #{name}."}
 end
 
-def assign_rooms(guest)
-  guest.map.with_index {|name, room|
-    "Hello, #{name}! You'll be assigned to room #{room+1}!"
-  }
+def assign_rooms(speakers)
+  speakers.map.with_index do
+    |name, room| "Hello, #{name}! You'll be assigned to room #{room+1}!"
+  end
 end
 
 def printer(guest)
-  batch_badge_creator(guest).map do |badge|
-    puts badge
-  end
-  assign_rooms(guest).map do |room|
-    puts room
-  end  
+  batch_badge_creator(guest).map {|badge| puts badge}
+  assign_rooms(guest).map {|room| puts room}
 end
